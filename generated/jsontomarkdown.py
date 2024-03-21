99aeca714fb5cf97a34f75281fa3feb91da2de79
Ylva Selling <ylva.selling@liu.se>
2024-03-21 17:03:07 +0100

    Generate docs for all asset components

--- generated/jsontomarkdown.py
+++ generated/jsontomarkdown.py
@@ -16,24 +16,26 @@ environment = Environment(loader=FileSystemLoader("../templates/"))
 assetComponentTemplate = environment.get_template("assetComponentTemplate.txt")
 indexTemplate = environment.get_template("indexTemplate.txt")
 
-category = assetCategories[2]
-# Find base class to add its members to each derived class
-baseclassName = category["name"]
-baseClass = None
-for x in category["classes"]:
-    if x["name"] == baseclassName:
-        baseClass = x
-        break
-for assetComponent in category["classes"]:
-    members = baseClass["members"]
-    if not assetComponent["name"] == baseclassName:
-        members = members + assetComponent["members"]
-        print(assetComponent["name"])
-    outputAssetComponent = assetComponentTemplate.render(data=assetComponent, members=members)
-    with open(assetComponent["name"]+'.md', 'w') as f:
-        f.write(outputAssetComponent)
+for category in assetCategories:
+    # Find base class to add its members to each derived class
+    baseclassName = category["name"]
+    baseClass = None
+    for x in category["classes"]:
+        if x["name"] == baseclassName:
+            baseClass = x
+            break
+    for assetComponent in category["classes"]:
+        members = []
+        if baseClass != None and "members" in baseClass.keys() and assetComponent["name"] != baseclassName:
+            members += baseClass["members"] 
+        if  "members" in assetComponent.keys():
+            members += assetComponent["members"]
 
-outputIndex = indexTemplate.render(category=category)
+        outputAssetComponent = assetComponentTemplate.render(data=assetComponent, members=members)
+        with open(assetComponent["name"]+'.md', 'w') as f:
+            f.write(outputAssetComponent)
+
+outputIndex = indexTemplate.render(assetCategories=assetCategories)
 with open('index.md', 'w') as f:
     f.write(outputIndex)
 
