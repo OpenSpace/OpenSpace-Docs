e542a513368aae209efd6a14d2f43d4c753ce8f2
Ylva Selling <ylva.selling@liu.se>
2024-03-27 13:54:39 +0100

    Change to new structure of json file where all components have a members variable

--- generated/jsontomarkdown.py
+++ generated/jsontomarkdown.py
@@ -5,12 +5,12 @@ import os # file path magic
 from jinja2 import Environment, FileSystemLoader # template magic
 
 # Opening JSON file
-f = open('documentationData.json')
+f = open('assetComponents.json')
 
 # Convert JSON String to Python Dictionary
 documentation_data = json.load(f)
 
-assetCategories = documentation_data["documentation"][7]["data"]
+assetCategories = documentation_data["data"]
 
 environment = Environment(loader=FileSystemLoader("../templates/"))
 assetComponentTemplate = environment.get_template("assetComponentTemplate.txt")
@@ -20,16 +20,27 @@ for category in assetCategories:
     # Find base class to add its members to each derived class
     baseclassName = category["name"]
     baseClass = None
-    for x in category["classes"]:
-        if x["name"] == baseclassName:
-            baseClass = x
+    hasBaseClass = False
+    for assetComponent in category["classes"]:
+        if assetComponent["name"] == baseclassName:
+            baseClass = assetComponent
+            hasBaseClass = True
             break
+    
+    # Go through all the components in that category and print out a md file
     for assetComponent in category["classes"]:
         members = []
-        if baseClass != None and "members" in baseClass.keys() and assetComponent["name"] != baseclassName:
-            members += baseClass["members"] 
-        if  "members" in assetComponent.keys():
-            members += assetComponent["members"]
+        # Add the base class members to each derived class
+        if not hasBaseClass:
+            members = assetComponent["members"]
+        # If the component is the base class, don't add the base class members on top of it
+        # (it would be duplicates)
+        elif hasBaseClass and assetComponent["name"] == baseClass["name"]:
+            members = baseClass["members"]
+        # For derived classes, add the base class members first
+        else:
+            members = baseClass["members"] + assetComponent["members"]
+        
 
         outputAssetComponent = assetComponentTemplate.render(data=assetComponent, members=members)
         with open(assetComponent["name"]+'.md', 'w') as f:
@@ -38,6 +49,7 @@ for category in assetCategories:
 outputIndex = indexTemplate.render(assetCategories=assetCategories)
 with open('index.md', 'w') as f:
     f.write(outputIndex)
+
 
 # Print Dictionary
 # print(documentation_data)
