c852b7521fe0089919ae832ed1838be23f0901fb
Ylva Selling <ylva.selling@liu.se>
2024-03-28 13:39:32 +0100

    Add scripting api to the generated documentation

--- generated/jsontomarkdown.py
+++ generated/jsontomarkdown.py
@@ -4,18 +4,27 @@ import os # file path magic
 
 from jinja2 import Environment, FileSystemLoader # template magic
 
+# Load jinja templates folder
+environment = Environment(loader=FileSystemLoader("../templates/"))
+
+################################################################################
+#                              ASSET COMPONENTS                                #
+################################################################################
+
+# Create target folder
+folderNameAssets = "assetComponents"
+if not os.path.exists("./" + folderNameAssets):
+    os.mkdir("./" + folderNameAssets)
+
 # Opening JSON file
 f = open('assetComponents.json')
 
 # Convert JSON String to Python Dictionary
 documentation_data = json.load(f)
-
 assetCategories = documentation_data["data"]
 
-environment = Environment(loader=FileSystemLoader("../templates/"))
+# Create pages for the asset component pages
 assetComponentTemplate = environment.get_template("assetComponentTemplate.txt")
-indexTemplate = environment.get_template("indexTemplate.txt")
-
 for category in assetCategories:
     # Find base class to add its members to each derived class
     baseclassName = category["name"]
@@ -43,13 +52,52 @@ for category in assetCategories:
         
 
         outputAssetComponent = assetComponentTemplate.render(data=assetComponent, members=members)
-        with open(assetComponent["name"]+'.md', 'w') as f:
+        with open(folderNameAssets + '/' + assetComponent["name"]+'.md', 'w') as f:
             f.write(outputAssetComponent)
 
-outputIndex = indexTemplate.render(assetCategories=assetCategories)
-with open('index.md', 'w') as f:
+# Create index file
+indexAssetComponentsTemplate = environment.get_template("indexAssetComponentsTemplate.txt")
+outputIndex = indexAssetComponentsTemplate.render(assetCategories=assetCategories)
+with open(folderNameAssets + '/index.md', 'w') as f:
     f.write(outputIndex)
 
 
-# Print Dictionary
-# print(documentation_data)
+################################################################################
+#                                 SCRIPTING API                                #
+################################################################################
+
+# Create target folder
+folderNameScripting = "scriptingApi"
+if not os.path.exists("./" + folderNameScripting):
+    os.mkdir("./" + folderNameScripting)
+
+# Opening JSON file
+f = open('scriptingApi.json')
+
+# Convert JSON String to Python Dictionary
+documentation_data = json.load(f)
+scriptingApi = documentation_data["data"]
+
+# Create the pages for the scripting libraries
+scriptingApiTemplate = environment.get_template("scriptingApiTemplate.txt")
+for library in scriptingApi:
+    # Go through all the functions in that library and print out a md file
+    outputLibrary = scriptingApiTemplate.render(library=library)
+    with open(folderNameScripting + "/" + library["fullName"]+'.md', 'w') as f:
+        f.write(outputLibrary)
+
+# Create index file
+indexScriptingTemplate = environment.get_template("indexScriptingTemplate.txt")
+outputIndex = indexScriptingTemplate.render(libraries=scriptingApi)
+with open(folderNameScripting + "/" + "index.md", 'w') as f:
+    f.write(outputIndex)
+
+################################################################################
+#                             CREATE INDEX FILE                                #
+################################################################################
+
+indexTemplate = environment.get_template("indexTemplate.txt")
+
+outputIndex = indexTemplate.render(folderNameScripting=folderNameScripting, folderNameAssets=folderNameAssets)
+with open("index.md", 'w') as f:
+    f.write(outputIndex)
