932b37dfea95b3450dee4bd329a5ac779f7a39be
Ylva Selling <ylva.selling@gmail.com>
2024-11-11 16:25:44 +0100

    Make references an argument and format the code

--- contribute/documentation/customdirectives.md
+++ contribute/documentation/customdirectives.md
@@ -7,10 +7,9 @@ We have created custom Sphinx directives for the documentation.
 :assetfile: data/assets/scene/digitaluniverse/allsky_hydrogenalpha.asset
 :openspaceversion: 1
 :preparedby: Doug Finkbeiner (Princeton), Brian Abbott (AMNH)
-:sourceversion: 2.02
-:license: mit
-
-[A Composite H-alpha Template for Microwave Foreground Prediction](https://doi.org/10.1086/374411)
+:sourceversion: 2.01
+:license: cc-by
+:reference: A Composite H-alpha Template for Microwave Foreground Prediction=https://doi.org/10.1086/374411;Bar=https://example.org/bar;John Doe
 :::
 
 To use this, you need a Sphinx version of at least [7.4](https://www.sphinx-doc.org/en/master/changes/7.4.html). To upgrade, run
@@ -27,12 +26,10 @@ The example above was created with the following code:
 :assetfile: data/assets/scene/digitaluniverse/allsky_hydrogenalpha.asset
 :openspaceversion: 1
 :preparedby: Doug Finkbeiner (Princeton), Brian Abbott (AMNH)
-:sourceversion: 2.02
-:license: amnh
-
-[A Composite H-alpha Template for Microwave Foreground Prediction](https://doi.org/10.1086/374411)
+:sourceversion: 2.01
+:license: cc-by
+:reference: A Composite H-alpha Template for Microwave Foreground Prediction=https://doi.org/10.1086/374411;Bar=https://example.org/bar;John Doe
 :::
 ```
 
-All arguments are optional. The references are placed in the content of the directive.
-This is so we can include links in the references. License can be one of: `amnh`, `cc-by`, and `mit`.
+All arguments are optional. The references are separated by `;` and can be either a name with a link or just a name. If the name has a link, place the name like so: `name=link`. The link can be an internal link to the docs page or an external link. License can be one of: `amnh`, `cc-by`, and `mit`.
--- directives.py
+++ directives.py
@@ -3,62 +3,77 @@ from jinja2 import Environment, FileSystemLoader
 from sphinx.util.docutils import SphinxDirective
 
 class Dossier(SphinxDirective):
-    has_content = True
+  has_content = True
 
-    licenses = {
-      "amnh": {
-        "name": "AMNH's Digital Universe",
-        "link": "https://www.amnh.org/research/hayden-planetarium/digital-universe/download/digital-universe-license"
-      },
-      "mit": {
-        "name": "MIT",
-        "link": "https://github.com/OpenSpace/OpenSpace/blob/master/LICENSE.md"
-      },
-      "cc-by": {
-        "name": "CC-BY",
-        "link": "https://creativecommons.org/licenses/by/4.0/"
-      },
-    }
+  licenses = {
+    "amnh": {
+      "name": "AMNH's Digital Universe",
+      "link": "https://www.amnh.org/research/hayden-planetarium/digital-universe/download/digital-universe-license"
+    },
+    "mit": {
+      "name": "MIT",
+      "link": "https://github.com/OpenSpace/OpenSpace/blob/master/LICENSE.md"
+    },
+    "cc-by": {
+      "name": "CC-BY",
+      "link": "https://creativecommons.org/licenses/by/4.0/"
+    },
+  }
 
-    option_spec = {
-      "census": unchanged, # Expects a string
-      "assetfile": unchanged, # Expects a string
-      "openspaceversion": unchanged, # Expects a string
-      "preparedby": unchanged, # Expects a string
-      "sourceversion": unchanged, # Expects a string
-      "license": lambda x: choice(x, ["amnh", "mit", "cc-by"]), # Expects a string, one of these
-    }
+  option_spec = {
+    "census": unchanged, # Expects a string
+    "assetfile": unchanged, # Expects a string
+    "openspaceversion": unchanged, # Expects a string
+    "preparedby": unchanged, # Expects a string
+    "sourceversion": unchanged, # Expects a string
+    "license": lambda x: choice(x, ["amnh", "mit", "cc-by"]), # Expects a string, one of these
+    "reference": unchanged
+  }
 
-    def run(self):
-        notapplicable = "Not applicable"
-        census = self.options.get("census", notapplicable)
-        assetfile = self.options.get("assetfile", notapplicable)
-        openspaceversion = self.options.get("openspaceversion", notapplicable)
-        preparedby = self.options.get("preparedby", notapplicable)
-        sourceversion = self.options.get("sourceversion", notapplicable)
-        license = self.options.get("license", notapplicable)
+  def process_reference(self, r):
+    if ("=" in r):
+      [name, link] = r.split("=")
+      return {
+        "name": name,
+        "link": link
+      }
+    else:
+      return { "name": r }
 
-        environment = Environment(loader=FileSystemLoader("templates"))
+  def run(self):
+    notapplicable = "Not applicable"
+    census = self.options.get("census", notapplicable)
+    assetfile = self.options.get("assetfile", notapplicable)
+    openspaceversion = self.options.get("openspaceversion", notapplicable)
+    preparedby = self.options.get("preparedby", notapplicable)
+    sourceversion = self.options.get("sourceversion", notapplicable)
+    license = self.options.get("license", notapplicable)
+    reference = self.options.get("reference", notapplicable)
 
-        # We can't pass free text as a parameter to the directive, so instead we
-        # add the ability to add free text for the references through the content of the
-        # directive. This is an array of strings.
-        reference = " ".join(self.content) if len(self.content) > 0 else notapplicable
-        license = self.licenses[license] if self.licenses.__contains__(license) else license
-        assetfile = f'`{assetfile}`' if assetfile != notapplicable else assetfile
-        template = environment.get_template("dossier.html.jinja")
+    if (reference != notapplicable):
+      reference = reference.split(";")
+      reference = list(map(self.process_reference, reference))
 
-        # Render component page with jinja
-        output = template.render(
-          census=census,
-          assetfile=assetfile,
-          openspaceversion=openspaceversion,
-          preparedby=preparedby,
-          sourceversion=sourceversion,
-          reference=reference,
-          license=license
-        )
+    environment = Environment(loader=FileSystemLoader("templates"))
 
-        self.content = output
-        allow_headers = True
-        return self.parse_content_to_nodes(allow_headers)
+    # We can't pass free text as a parameter to the directive, so instead we
+    # add the ability to add free text for the references through the content of the
+    # directive. This is an array of strings.
+    license = self.licenses[license] if self.licenses.__contains__(license) else license
+    assetfile = f'`{assetfile}`' if assetfile != notapplicable else assetfile
+    template = environment.get_template("dossier.html.jinja")
+
+    # Render component page with jinja
+    output = template.render(
+      census=census,
+      assetfile=assetfile,
+      openspaceversion=openspaceversion,
+      preparedby=preparedby,
+      sourceversion=sourceversion,
+      reference=reference,
+      license=license
+    )
+
+    self.content = output
+    allow_headers = True
+    return self.parse_content_to_nodes(allow_headers)
